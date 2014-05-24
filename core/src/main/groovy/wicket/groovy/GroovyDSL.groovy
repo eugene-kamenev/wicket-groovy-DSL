ec60f1137ac335a3d1bb9c07a612b7ef1501c0bd
eugene <ekamenev@bitbucket.org>
2014-05-25 00:50:17 +0600

    adding support for image, statless form and other components

--- core/src/main/groovy/wicket/groovy/GroovyDSL.groovy
+++ core/src/main/groovy/wicket/groovy/GroovyDSL.groovy
@@ -5,10 +5,13 @@ import org.apache.wicket.MarkupContainer
 import org.apache.wicket.Page
 import org.apache.wicket.ajax.markup.html.AjaxLink
 import org.apache.wicket.markup.html.basic.Label
+import org.apache.wicket.markup.html.form.Form
+import org.apache.wicket.markup.html.form.StatelessForm
+import org.apache.wicket.markup.html.image.Image
 import org.apache.wicket.markup.html.link.BookmarkablePageLink
+import org.apache.wicket.markup.html.list.ListView
 import org.apache.wicket.model.IModel
-import wicket.groovy.core.GroovyAjaxLink
-import wicket.groovy.core.GroovyWebMarkupContainer
+import wicket.groovy.core.*
 
 class GroovyDSL {
 
@@ -22,7 +25,7 @@ class GroovyDSL {
 
     static <T> MarkupContainer div(Component parent, String id, IModel<T> model, Map<String, Closure> overrides = null, Closure closure = null) {
         def panel = new GroovyWebMarkupContainer(id, model, overrides)
-        parent.add panel
+        parent?.add panel
         closure?.call(panel)
         panel
     }
@@ -30,21 +33,21 @@ class GroovyDSL {
 
     static BookmarkablePageLink bookmarkLink(Component parent, String id, Class<? extends Page> target, Closure closure = null) {
         def link = new BookmarkablePageLink(id, target)
-        parent.add link
+        parent?.add link
         closure?.call(link)
         link
     }
 
     static <T> Label label(Component parent, String id, IModel<T> model = null, Closure closure = null) {
         def child = new Label<T>(id, model)
-        parent.add child
+        parent?.add child
         closure?.call(child)
         child
     }
 
     static <T> AjaxLink ajaxLink(Component parent, String id, IModel<T> model, Map<String, Closure> overrides = null, Closure closure = null) {
         def child = new GroovyAjaxLink(id, model, overrides)
-        parent.add child
+        parent?.add child
         closure?.call(child)
         child
     }
@@ -53,7 +56,39 @@ class GroovyDSL {
         ajaxLink(parent, id, null, overrides, closure)
     }
 
+    static <T> Form<T> form(Component parent, String id, IModel<T> model = null, Map<String, Closure> overrides = null, Closure closure = null) {
+        def form = new GroovyForm<T>(id, model, overrides)
+        parent?.add form
+        closure?.call(form)
+        form
+    }
+
+    static <T> StatelessForm<T> statelessForm(Component parent, String id, IModel<T> model = null, Map<String, Closure> overrides = null, Closure closure = null) {
+        def form = new GroovyStatelessForm<T>(id, model, overrides)
+        parent?.add form
+        closure?.call(form)
+        form
+    }
+
+    static <T> ListView<T> listView(Component parent, String id, IModel<? extends List<T>> model = null, Map<String, Closure> overrides = null, Closure closure = null) {
+        def listView = new GroovyListView<T>(id, model, overrides, closure)
+        parent?.add listView
+        listView
+    }
+
+    static <T> ListView<T> listView(List<T> list, String id, Map<String, Closure> overrides = null, Closure closure) {
+        def listView = new GroovyListView<T>(id, list, overrides, closure)
+        listView
+    }
+
+    static <T> Image image(Component parent, String id, IModel<T> model = null, Map<String, Closure> overrides = null, Closure closure = null) {
+        def image = new GroovyImage<T>(id, model, overrides)
+        parent?.add image
+        closure?.call(image)
+        image
+    }
+
     static leftShift(Component parent, Component... child) {
-        parent.add child
+        parent?.add child
     }
 }
